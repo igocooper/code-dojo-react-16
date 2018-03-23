8389456f068121dec02f511a9ac3b8e9e9534db6
Oleg Lytvyn <igocooper@zeoalliance.com>
2018-03-23 13:04:07 +0200

    Portal introduces

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 import './App.css';
 import logo from './logo.svg';
+import Modal from './components/Modal';
 
 export default class App extends Component {
+    state = {
+        isModalOpen: false
+    }
+
+    toggleModal = () => {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    };
     render() {
         return (
             <div className="app">
@@ -11,10 +19,13 @@ export default class App extends Component {
                     <img src={logo} alt="React logo" />
                     <h1>React 16</h1>
                 </header>
-
-                <main>
-                    
-                </main>
+                <button onClick={this.toggleModal}>Открыть</button>
+                {this.state.isModalOpen && 
+                    // modal will open itself in the portal
+                    <main>
+                        <Modal toggleModal={this.toggleModal} />
+                    </main>
+                }
             </div>
         );
     }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './Modal.css';
 
 export default class Modal extends React.Component {
+
+    componentWillMount() {
+        // create container entrypoint and add it to the dom
+        this.portal = document.createElement('div');
+        document.body.appendChild(this.portal);
+    }
+
+    componentWillUnmount() {
+        // remove entrypoint when conponent is no more in use
+        document.body.removeChild(this.portal)
+    }
+  
     render() {
-        return (
-            <div className="modal">
-                <button className="modal__close-button">Закрыть</button>
-                {this.props.children}
-            </div>
-        );
+        return ReactDOM.createPortal(
+                (<div className="modal">
+                    <button className="modal__close-button" onClick={this.props.toggleModal}>Закрыть</button>
+                    {this.props.children}
+                </div>),
+                this.portal
+            )
+            
     }
 }
